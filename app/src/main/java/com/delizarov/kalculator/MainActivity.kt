a3cda12630b3e5f970cd6857d216fefa0069f2f2
TheRoom101 <the-room-101@yandex-team.ru>
2019-04-29 16:31:20 +0300

    statusbar color

--- app/src/main/java/com/delizarov/kalculator/MainActivity.kt
+++ app/src/main/java/com/delizarov/kalculator/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.delizarov.kalculator
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.mine_shaft)
     }
 }
